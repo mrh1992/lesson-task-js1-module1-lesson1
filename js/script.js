c9dc02d88022c4f41a1a724b18916b1fbafd963b
mrh1992 <71317905+mrh1992@users.noreply.github.com>
2021-01-20 11:26:34 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//Question 1
+
+const instrument = "guitar";
+console.log(instrument);
+
+//Question 2
+
+const button = document.querySelector("button.login");
+console.dir(button);
+
+//Question 3
+
+const instruments = [
+    {
+        type: "guitar",
+        colour: "red",
+    },
+    {
+        type: "piano",
+        colour: "black",
+    },
+];
+
+console.table(instruments);
+
+//Question 4
+
+const name = "Percival";
+const age = 13;
+
+//Question 5
+
+let total = 0;
+total = total + 10;
+
+//Question 6
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+//Question 7
+
+const firstName = "Florence";
+
+const introduction = 'Hello, my name is ${firstName}.';
+
+console.log(introduction);
+
+//Question 8
+
+const title = "Big Technical Event";
+
+const whatToHave = "good time";
+
+const welcome = 'Welcome! This ${title} is starting today. Have a ${whatToHave}!';
+
+console.log(welcome);
+
+//Question 9
+
+//Question 10
